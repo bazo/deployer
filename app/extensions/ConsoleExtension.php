705b73f91be76f88622c454ac4220ac84b6cff0c
Martin Bazik <martin@bazo.sk>
2013-06-13 23:36:40 +0200

    updated console

--- app/extensions/ConsoleExtension.php
+++ app/extensions/ConsoleExtension.php
@@ -2,7 +2,7 @@
 
 namespace Extensions;
 
-use Nette\Framework;
+use \App;
 
 /**
  * Console service.
@@ -13,58 +13,63 @@ class ConsoleExtension extends \Nette\DI\CompilerExtension
 {
 
 	/**
-     * Processes configuration data
-     *
-     * @return void
-     */
-    public function loadConfiguration() {
-        $container = $this->getContainerBuilder();
+	 * Processes configuration data
+	 *
+	 * @return void
+	 */
+	public function loadConfiguration()
+	{
+		$container = $this->getContainerBuilder();
 
-        // console application
-        $container->addDefinition($this->prefix('console'))
-                  ->setClass('Symfony\Component\Console\Application')
-                  ->setFactory('Extensions\ConsoleExtension::createConsole', array('@container'))
-                  ->setAutowired(false);
+		// console application
+		$container->addDefinition($this->prefix('console'))
+				->setClass('Symfony\Component\Console\Application')
+				->setFactory('Extensions\ConsoleExtension::createConsole', array('@container'))
+				->setAutowired(false);
 
-        // aliases
-        $container->addDefinition('console')
-                  ->setClass('Symfony\Component\Console\Application')
-                  ->setFactory('@container::getService', array($this->prefix('console')));
+		// aliases
+		$container->addDefinition('console')
+				->setClass('Symfony\Component\Console\Application')
+				->setFactory('@container::getService', array($this->prefix('console')));
 
-		
+
 		$this->compiler->parseServices($container, $this->loadFromFile($container->expand('%appDir%') . '/config/console.neon'), 'console');
 	}
 
-    /**
-     * @param \Nette\DI\Container
-     * @param \Symfony\Component\Console\Helper\HelperSet
-     * @return \Symfony\Component\Console\Application
-     */
-    public static function createConsole(\Nette\DI\Container $container, \Symfony\Component\Console\Helper\HelperSet $helperSet = null) {
-        $console = new \Symfony\Component\Console\Application(Framework::NAME . " Command Line Interface", Framework::VERSION);
 
-        if (!$helperSet) {
-            $helperSet = new \Symfony\Component\Console\Helper\HelperSet;
+	/**
+	 * @param \Nette\DI\Container
+	 * @param \Symfony\Component\Console\Helper\HelperSet
+	 * @return \Symfony\Component\Console\Application
+	 */
+	public static function createConsole(\Nette\DI\Container $container, \Symfony\Component\Console\Helper\HelperSet $helperSet = null)
+	{
+		$console = new \Symfony\Component\Console\Application(App::NAME . " Command Line Interface", App::VERSION);
 
-            foreach (array_keys($container->findByTag('consoleHelper')) as $helperName) {
+		if (!$helperSet) {
+			$helperSet = new \Symfony\Component\Console\Helper\HelperSet;
+
+			foreach (array_keys($container->findByTag('consoleHelper')) as $helperName) {
 				$helper = $container->getService($helperName);
 
 				$helperName = substr($helperName, $start = 8); //console. - 8 cahrs
 
 				$helperSet->set($helper, $helperName);
 			}
-        }
+		}
 
-        $console->setHelperSet($helperSet);
-        $console->setCatchExceptions(false);
+		$console->setHelperSet($helperSet);
+		$console->setCatchExceptions(false);
 
-        $commands = array();
-        foreach (array_keys($container->findByTag('consoleCommand')) as $name) {
-            $commands[] = $container->getService($name);
-        }
-        $console->addCommands($commands);
+		$commands = array();
+		foreach (array_keys($container->findByTag('consoleCommand')) as $name) {
+			$commands[] = $container->getService($name);
+		}
+		$console->addCommands($commands);
 
-        return $console;
-    }
+		return $console;
+	}
+
+
 }
 
